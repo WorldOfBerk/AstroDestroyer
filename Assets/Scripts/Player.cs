1b58d0e774d76c0cc42253ea16f0374857b96698
WorldOfBerk <baris.berk.sengl@hotmail.com>
2022-10-20 22:16:09 +0300

    PlayerMovement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody;
+    public float thrustSpeed = 1.0f;
+    public float turnSpeed = 1.0f;
+    private bool _thrusting;
+    private float _turnDirection;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        _thrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            _turnDirection = 1.0f;
+        }
+        else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+
+        {
+            _turnDirection = -1.0f;
+        }
+        else
+        {
+            _turnDirection = 0.0f;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_thrusting)
+        {
+            _rigidbody.AddForce(this.transform.up * this.thrustSpeed);
+        }
+
+        if (_turnDirection != 0.0f)
+        {
+            _rigidbody.AddTorque(_turnDirection * this.turnSpeed);
+        }
     }
 }
