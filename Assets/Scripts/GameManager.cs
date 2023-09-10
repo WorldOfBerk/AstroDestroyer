351aa622d3e581b696ecd6122d99c841a5ed4f37
WorldOfBerk <baris.berk.sengl@hotmail.com>
2023-09-11 01:23:10 +0300

    UI section added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
    public Player player;
    public ParticleSystem explosion;
    public float respawnTime = 3.0f;
+   [Header("Numerical")]
    public float respawnReActiveTime = 3.0f;
    public int lives = 3;
    public int score = 0;
+   [SerializeField] private int winScore;
+   [Header("UI")]
+   public TMP_Text scoreText;
+   public Image[] heartImages;
+   public Sprite heartSprite;
+   public Sprite brokenHeartSprite;
+   public GameObject losePanel;
+   public GameObject winPanel;
+   public bool isGameOver = false;
+   
+   
 
+   private void Start()
+   {
+      scoreText = GameObject.Find("ScoreText").GetComponent<TMP_Text>();
+      UpdateScoreText();
+      UpdateLivesUI();
+   }
+
+   void UpdateScoreText()
+   {
+      scoreText.text = "Score: " + score.ToString();
+   }
+
+   void UpdateLivesUI()
+   {
+      for (int i = 0; i < heartImages.Length; i++)
+      {
+         if (i < lives)
+         {
+            heartImages[i].sprite = heartSprite;
+         }
+         else
+         {
+            heartImages[i].sprite = brokenHeartSprite;
+         }
+      }
+   }
+   
    public void AsteroidDestroyed(Asteroid asteroid)
    {
       explosion.transform.position = asteroid.transform.position;
@@ -28,6 +70,13 @@ public class GameManager : MonoBehaviour
       {
          score += 25;
       }
+      
+      UpdateScoreText();
+
+      if (score >= winScore && !isGameOver)
+      {
+         WinGame();
+      }
    }
 
    public void PlayerDied()
@@ -44,6 +93,7 @@ public class GameManager : MonoBehaviour
 
       else
       {
+         UpdateLivesUI();
          Invoke(nameof(Respawn), respawnTime);
       }
    }
@@ -64,6 +114,17 @@ public class GameManager : MonoBehaviour
 
    private void GameOver()
    {
-      
+      isGameOver = true;
+      losePanel.SetActive(true);
+      Cursor.visible = true;
+      Cursor.lockState = CursorLockMode.None;
+   }
+
+   private void WinGame()
+   {
+      isGameOver = true;
+      winPanel.SetActive(true);
+      Cursor.visible = true;
+      Cursor.lockState = CursorLockMode.None;
    }
 }
